9270421260aaf1f5131e9b48c81618e29d3e6993
Raj <khandor.raj@gmail.com>
2020-04-23 16:15:28 +0530

    Adding face coordinates as another feature in calculating face similarity distance

--- utility.py
+++ utility.py
@@ -6,6 +6,7 @@ import subprocess
 import os
 from pydub import AudioSegment
 from facenet_pytorch import MTCNN, InceptionResnetV1, extract_face
+from numpy.linalg import norm
 
 
 class Audio_Processing:
@@ -79,7 +80,7 @@ class Video_Processing:
 			if self.device.type == "cuda":
 				cropped_tensors = cropped_tensors.cpu()
 			cropped_tensors = cropped_tensors.numpy()
-			self.embeddings_initial.append(cropped_tensors)
+			self.embeddings_initial.append((cropped_tensors,((x1+x2)/2, (y1+y2)/2)))
 			self.total_people += 1
 
 		# Seperate video with no audio
@@ -100,13 +101,21 @@ class Video_Processing:
 		count = 0 
 		for frame in frames:
 			bouding_box, prob = self.mtcnn.detect(frame)
+			bouding_box = [x for _,x in sorted(zip(prob,bouding_box), reverse=True)] #Sorting reverse as per prob
 
-			for box in bouding_box:
+			f1 = frame.copy()
+			draw = ImageDraw.Draw(f1)
+
+			for box in bouding_box[:self.total_people]:
 				x1,y1,x2,y2 = box
 				if(x1 > x2):
 					x1, x2 = x2, x1
 				if(y1 > y2):
 					y1, y2 = y2, y1
+
+				pos = ((x1+x2)/2, (y1+y2)/2)
+
+				draw.rectangle(box.tolist(), outline=(255, 0, 0), width=3)
 
 				cropped_tensors = extract_face(frame, (x1,y1,x2,y2)).to(self.device).view(-1,3,160,160)
 				emb = self.resnet(cropped_tensors)
@@ -118,11 +127,17 @@ class Video_Processing:
 				idx = -1
 				min_dist = 10**9
 				for i,e in enumerate(self.embeddings_initial):
-					d = emb-e
+					d = emb-e[0]
 					d = d.reshape(512)
 
 					# https://github.com/cmusatyalab/openface/blob/master/demos/compare.py
 					dist = np.dot(d,d)  # https://cmusatyalab.github.io/openface/demo-2-comparison/
+					x11, y11 = e[1]
+					x22, y22 = pos
+
+					# Adding spatial information
+					dist += abs(x11-x22) + abs(y11-y22)
+					#print(dist, min_dist)
 					if(dist < min_dist):
 						idx = i
 						min_dist = dist
@@ -135,7 +150,13 @@ class Video_Processing:
 				cv2.imshow(str(idx), crop)
 				cv2.waitKey(1)
 				
+			cv2.imshow("Frames", cv2.cvtColor(np.array(f1), cv2.COLOR_RGB2BGR))
+			cv2.waitKey(1)
 			print(len(a[0]))
+
+
+		for i in range(self.total_people):
+			print(len(a[i]))
 		return a
 
 
